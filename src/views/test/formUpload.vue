a8c57e5f9e6bfcfaba03de66af389d7265644893
fangbao <fangbao@wanglibank.com>
2017-06-26 15:50:58 +0800

    eidt

--- src/views/test/formUpload.vue
+++ src/views/test/formUpload.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
+  <div class="wrap">
     <input type="file" />
   </div>
 </template>
+<style lang="sass" scoped>
+  .wrap
+    display: flex
+    justify-content: center
+    align-items: center
+    flex: 1
+</style>
