4e09eb639dc69ef7a0f6601142cbc5788df6d9b3
fangbao <fangbao@wanglibank.com>
2017-07-09 23:56:44 +0800

    edit

--- src/containers/Topic.js
+++ src/containers/Topic.js
@@ -42,39 +42,37 @@ class Topic extends Component {
   }
   componentDidMount () {
     console.log(styles['list-view'], 'list-view')
-    // var that = this
-    // window.mui.init({
-    //   pullRefresh: {
-    //     container: '.' + styles['list-view'], // 下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
-    //     down: {
-    //       style: 'circle', // 必选，下拉刷新样式，目前支持原生5+ ‘circle’ 样式
-    //       color: '#2BD009', // 可选，默认“#2BD009” 下拉刷新控件颜色
-    //       height: 50, // 可选,默认50.触发下拉刷新拖动距离,
-    //       auto: false, // 可选,默认false.首次加载自动上拉刷新一次
-    //       callback: function () {
-    //         setTimeout(() => {
-    //           window.mui('.' + styles['list-view']).pullRefresh().pulldown = false
-    //         }, 1000)
-    //       } // 必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
-    //     }
-    //   }
-    // })
-    // window.mui.init({
-    //   pullRefresh: {
-    //     container: '.' + styles['list-view'], // 待刷新区域标识，querySelector能定位的css选择器均可，比如：id、.class等
-    //     up: {
-    //       height: 50, // 可选.默认50.触发上拉加载拖动距离
-    //       auto: true, // 可选,默认false.自动上拉加载一次
-    //       contentrefresh: '正在加载...', // 可选，正在加载状态时，上拉加载控件上显示的标题内容
-    //       contentnomore: '没有更多数据了', // 可选，请求完毕若没有更多数据时显示的提醒内容；
-    //       callback: function () {
-    //         setTimeout(() => {
-    //           window.mui('.' + styles['list-view']).pullRefresh().pullup = false
-    //         }, 1000)
-    //       } // 必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
-    //     }
-    //   }
-    // })
+    var that = this
+    window.mui.init({
+      pullRefresh: {
+        container: '.am-tabs-tabpane-active', // 下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+        down: {
+          style: 'circle', // 必选，下拉刷新样式，目前支持原生5+ ‘circle’ 样式
+          color: '#2BD009', // 可选，默认“#2BD009” 下拉刷新控件颜色
+          height: 50, // 可选,默认50.触发下拉刷新拖动距离,
+          auto: false, // 可选,默认false.首次加载自动上拉刷新一次
+          callback: function () {
+            setTimeout(() => {
+              this.endPulldownToRefresh(true)
+            }, 1000)
+          } // 必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+        },
+        up: {
+          style: 'circle',
+          height: 0, // 可选.默认50.触发上拉加载拖动距离
+          auto: false, // 可选,默认false.自动上拉加载一次
+          contentrefresh: '正在加载...', // 可选，正在加载状态时，上拉加载控件上显示的标题内容
+          contentnomore: '没有更多数据了', // 可选，请求完毕若没有更多数据时显示的提醒内容；
+          callback: function () {
+            console.log('up', window.mui('.' + styles['list-view']).pullRefresh())
+            setTimeout(() => {
+              this.endPullupToRefresh(false)
+            }, 1000)
+          } // 必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+        }
+      }
+    })
+    // window.mui('.' + styles['list-view']).pullRefresh().disablePullupToRefresh()
   }
   onEndReached () {
     console.log('end')
@@ -97,13 +95,23 @@ class Topic extends Component {
         <Tabs className="topic-container-tabs-bar" defaultActiveKey="1" onChange={callback} swipeable={false} animated={false} onTabClick={handleTabClick}>
           <TabPane tab="全部" key="1" >
             <div className={styles['list-view']}>
+              <div>
+                <TopicItem className="mb-18"/>
+                <TopicItem className="mb-18"/>
+                <TopicItem className="mb-18"/>
+                <TopicItem className="mb-18"/>
+                <TopicItem className="mb-18"/>
+                <TopicItem className="mb-18"/>
+                <TopicItem className="mb-18"/>
+                <TopicItem className="mb-18"/>
+              </div>
               {
-                topicList[typeid] && topicList[typeid].map(function (item, index) {
-                  console.log(item, 'item')
-                  return (
-                    (<TopicItem key={index} {...item} className="mb-18" />)
-                  )
-                })
+                // topicList[typeid] && topicList[typeid].map(function (item, index) {
+                //   console.log(item, 'item')
+                //   return (
+                //     (<TopicItem key={index} {...item} className="mb-18" />)
+                //   )
+                // })
                 /*
                   <ListView
                     dataSource={ds}
