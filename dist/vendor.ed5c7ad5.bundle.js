04171eb721f6f1dc9a7ec78605e66417798c0549
fangbao <fangbao@wanglibank.com>
2017-07-11 10:54:07 +0800

    build

---
+++ dist/vendor.ed5c7ad5.bundle.js
@@ -0,0 +1,27 @@
+webpackJsonp([5],{
+
+/***/ 130:
+/***/ (function(module, exports) {
+
+"use strict";
+eval("throw new Error(\"Module build failed: Error: ENOENT: no such file or directory, open '/Users/gaoyanru/Documents/demo/react-frame/node_modules/react-dom/index.js'\\n    at Error (native)\");//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIxMzAuanMiLCJzb3VyY2VSb290IjoiIn0=");
+
+/***/ }),
+
+/***/ 315:
+/***/ (function(module, exports) {
+
+"use strict";
+eval("throw new Error(\"Module build failed: Error: ENOENT: no such file or directory, open '/Users/gaoyanru/Documents/demo/react-frame/node_modules/babel-polyfill/lib/index.js'\\n    at Error (native)\");//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIzMTUuanMiLCJzb3VyY2VSb290IjoiIn0=");
+
+/***/ }),
+
+/***/ 8:
+/***/ (function(module, exports) {
+
+"use strict";
+eval("throw new Error(\"Module build failed: Error: ENOENT: no such file or directory, open '/Users/gaoyanru/Documents/demo/react-frame/node_modules/react/react.js'\\n    at Error (native)\");//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiI4LmpzIiwic291cmNlUm9vdCI6IiJ9");
+
+/***/ })
+
+});
