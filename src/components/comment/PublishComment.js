64a60d0c6471e3ed301594609cc218e6c5d80123
fangbao <fangbao@wanglibank.com>
2017-07-20 13:11:10 +0800

    build

--- src/components/comment/PublishComment.js
+++ src/components/comment/PublishComment.js
@@ -36,6 +36,7 @@ class PublishComment extends Component {
     $('textarea').blur(() => {
       $(el).css({position: 'initial'})
     })
+    alert(plus)
     document.addEventListener('plusready', plusReady, false)
   }
   toCancel () {
