563d7e04d194a1af343067434d637c3c7ad763a5
fangbao <fangbao@wanglibank.com>
2017-07-21 14:58:17 +0800

    build

--- build/build.js
+++ build/build.js
@@ -1,23 +1,35 @@
 var rm = require('rimraf');
 const webpack = require('webpack');
-var webpackConfig = require('../webpack.config');
+var webpackConfig = require('./webpack.prod.config');
+
 var ora = require('ora');
 var spinner = ora('building for production...');
 spinner.start();
-rm('dist/*', function(){
-  var compiler = webpack(webpackConfig,(err, stats) => {
+// console.log(webpack, 'webpack');
+// return;
+rm('dist/*', function(err){
+  if (err) throw err;
+  webpack(webpackConfig, function(err, stats){
     spinner.stop();
-    console.log(stats.toString({
-      // chunks: true,  // Makes the build much quieter
-      colors: true    // Shows colors in the console
-    }));
+    if (err) throw err
+    process.stdout.write(stats.toString({
+      colors: true,
+      // modules: false,
+      // children: false,
+      // chunks: false,
+      // chunkModules: false
+    }) + '\n\n')
+    // console.log(stats.toString({
+    //   // chunks: true,  // Makes the build much quieter
+    //   colors: true    // Shows colors in the console
+    // }));
   })
-  var watching = compiler.watch({},(err, stats) => {
-    console.log(stats.toString({
-      colors: true
-    }));
-  })
-  watching.close(() => {
-    console.log('watching Ended.');
-  })
+  // var watching = compiler.watch({},(err, stats) => {
+  //   console.log(stats.toString({
+  //     colors: true
+  //   }));
+  // })
+  // watching.close(() => {
+  //   console.log('watching Ended.');
+  // })
 })
--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import Router from '@/router'
@@ -7,7 +8,7 @@ import 'stylus/app'
 import 'lib/mui/js/mui.zoom'
 import 'lib/mui/js/mui.previewimage'
 import '@/util/common'
-render(
-  <Router />,
-  document.getElementById('root')
-)
+// render(
+//   <Router />,
+//   document.getElementById('root')
+// )
