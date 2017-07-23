a3802c496c48a3eba0ff19b62a582c2804bc6319
fangbao <fangbao@wanglibank.com>
2017-07-23 09:28:28 +0800

    camera test

--- src/util/native.js
+++ src/util/native.js
@@ -1,6 +1,7 @@
 var native = {
   ready (app) {
     if (window.plus) {
+      plus.navigator.setStatusBarBackground('#000000')
       app && app(window.plus, 1)
     } else {
       document.addEventListener('plusready', function () {
