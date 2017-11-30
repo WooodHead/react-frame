bca1434b5c70359fc7b8623200ad9fa21a8c3253
丁亮 <dingliang@i-counting.cn>
2017-11-30 11:21:21 +0800

    nmsg

--- src/pages/declear/result.tsx
+++ src/pages/declear/result.tsx
@@ -1,27 +1,55 @@
 import React from 'react'
 import { connect } from 'react-redux'
 const styles = require('../../stylus/declear.result')
-import { Table,  Row, Col } from 'antd'
+import { Col, Row, Table  } from 'antd'
 import { TableColumnConfig } from 'antd/lib/table/Table'
 import DeclearContainer from '../../containers/DeclearContainer'
-class Result extends React.Component<any, {}> {
+// import { MyState } from 'src/pages/demo3';
+
+interface MyStates {
+  data: any[]
+}
+class Result extends React.Component<any, MyStates> {
   // public componentWillMount () {
   // }
+  constructor () {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+  public columns: TableColumnConfig<any>[] = [
+    { title: '序号', dataIndex: '', key: '0', className: 'id' },
+    { title: '征收项目名称', dataIndex: '', key: '1' ,
+      render: (text, record,index) => (
+        index+1
+      )
+    },
+    { title: '申报状态', dataIndex: '', key: '2' },
+    { title: '税款所属期起', dataIndex: '', key: '3' },
+    { title: '税款所属期止', dataIndex: '', key: '4' },
+    { title: '申报日期', dataIndex: '', key: '5' },
+    { title: '应补（退）金额', dataIndex: '', key: '6' },
+    { title: '应税凭证序号', dataIndex: '', key: '7' },
+    {
+      title: '操作',
+      key: 'operation',
+      render: (text, record) => (
+          <span>
+            <a href='#'>申报作废</a>
+          </span>
+      )
+    }
+  ]
   public render () {
     return (
       <DeclearContainer currentTitle='查看申报结果'>
-        <div className={styles.inner}>
-          <div className={styles.comInfo}>
-            <p className={styles.comName}>
-              北京海昌
-            </p>
-            <p className={styles.class}>
-              <Row>
-                
-              </Row>
-            </p>
-          </div>
-        </div>
+        <Table
+          className={styles.DRtable}
+          columns={this.columns}
+          dataSource={this.state.data}
+          bordered
+        ></Table>
       </DeclearContainer>
     )
   }
